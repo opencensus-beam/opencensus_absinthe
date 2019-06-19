cfbac56bbed25506daee51efdff313c72792f8d9
Daniel Flanagan <daniel.flanagan@postmates.com>
2019-06-19 16:31:19 -0700

    Fix dialyzer errors

--- lib/opencensus/absinthe/phase/schema_push.ex
+++ lib/opencensus/absinthe/phase/schema_push.ex
@@ -3,6 +3,9 @@ defmodule Opencensus.Absinthe.Phase.SchemaPush do
 
   use Absinthe.Phase
 
+  alias Absinthe.Blueprint
+  alias Absinthe.Phase
+
   @impl true
   @spec run(Blueprint.t(), keyword()) :: Phase.result_t()
   def run(blueprint, opts \\ []) do
